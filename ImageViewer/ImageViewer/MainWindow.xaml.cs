908e39d4f8e036332bbdf12ecd4b678d35e7409b
FreeSpirit18 <99819303+FreeSpirit18@users.noreply.github.com>
2023-07-19 20:56:30 +0300

    Data binding

--- ImageViewer/ImageViewer/MainWindow.xaml.cs
+++ ImageViewer/ImageViewer/MainWindow.xaml.cs
@@ -1,13 +1,44 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 
 namespace ImageViewer
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
+            DataContext = this;
+            display = emty;
             InitializeComponent();
         }
+        private string _display;
 
+        private readonly string emty = "C:\\Users\\admin\\Documents\\GitHub\\WPFimageViewer\\ImageViewer\\ImageViewer\\images\\No_Image_Available.jpg";
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string display
+        {
+            get { return _display; }
+            set { 
+                _display = value;
+                OnPropertyChange("display");
+            }
+        }
+
+        private void Select_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            display = emty;
+        }
+
+        private void OnPropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
