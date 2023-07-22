a98417544137ce98cd0bc3d7066350f115109475
FreeSpirit18 <99819303+FreeSpirit18@users.noreply.github.com>
2023-07-22 17:31:54 +0300

    mvvm done

---
+++ ImageViewer/ImageViewer/MVVM/ViewModel/MainWindowVM.cs
@@ -0,0 +1,103 @@
+﻿using ImageViewer.MVVM.Model;
+using ImageViewer.MVVM.ViewModel;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using WinForms = System.Windows.Forms;
+
+namespace ImageViewer.MVVM.ViewModel
+{
+    internal class MainWindowVM : ViewModelBase
+    {
+        
+        public MainWindowVM() {
+            DisplayImage = new Image("", emty);
+            Images = new ObservableCollection<Image>();
+            
+
+            SelectCommand = new RelayCommand(execute => Select_Click(), canExecute => { return true; });
+            ClearCommand = new RelayCommand(execute => Clear_Click(), canExecute => { return true; });
+        }
+        public RelayCommand SelectCommand { get; private set; }
+        public RelayCommand ClearCommand { get; private set; }
+
+        private readonly string emty = "C:\\Users\\admin\\Documents\\GitHub\\WPFimageViewer\\ImageViewer\\ImageViewer\\images\\No_Image_Available.jpg";
+
+        private Image displayImage;
+
+        public Image DisplayImage
+        {
+            get { return displayImage; }
+            set
+            {
+                displayImage = value;
+                OnPropertyChange("DisplayImage");
+            }
+        }
+        private ObservableCollection<Image> images;
+        public ObservableCollection<Image> Images
+        {
+            get { return images; }
+            set
+            {
+                images = value;
+                OnPropertyChange("Images");
+            }
+        }
+
+        private void Select_Click()
+        {
+            WinForms.FolderBrowserDialog dialog = new WinForms.FolderBrowserDialog();
+            
+            WinForms.DialogResult result = dialog.ShowDialog();
+
+            if (result == WinForms.DialogResult.OK)
+            {
+                string folder = dialog.SelectedPath;
+
+                string[] allFiles = Directory.GetFiles(folder);
+
+
+                string[] imageExtensions = { ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff" };
+
+                string[] ImagePaths = allFiles.Where(file => imageExtensions.Contains(Path.GetExtension(file).ToLower())).ToArray();
+
+                Images.Clear();
+                for (int i = 0; i < ImagePaths.Length; i++)
+                {
+                    Images.Add(new Image(Path.GetFileName(ImagePaths[i]), ImagePaths[i]));
+                }
+
+            }
+            else
+            {
+                //MessageBox.Show("Could not open image.", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+
+        private void Clear_Click()
+        {
+            DisplayImage = new Image("", emty);
+        }
+
+
+        private void folderImages_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            /*int imageIndex = folderImages.SelectedIndex;
+            if (imageIndex != -1)
+            {
+                DisplayImage = Images[imageIndex];
+            }
+            else
+            {
+                DisplayImage = new Image("", emty);
+            }*/
+
+        }
+    }
+}
--- ImageViewer/ImageViewer/MainWindow.xaml.cs
+++ ImageViewer/ImageViewer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Win32;
+﻿using ImageViewer.MVVM.ViewModel;
+using Microsoft.Win32;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
@@ -8,98 +9,18 @@ using WinForms = System.Windows.Forms;
 
 namespace ImageViewer
 {
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            DataContext = this;
-            Display = emty;
-            _folderImages = new ObservableCollection<string>();
-
 
             InitializeComponent();
+            MainWindowVM vm = new MainWindowVM();
+            DataContext = vm;
         }
         
 
-        
-
-        private readonly string emty = "C:\\Users\\admin\\Documents\\GitHub\\WPFimageViewer\\ImageViewer\\ImageViewer\\images\\No_Image_Available.jpg";
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-        
-
-
-        
-        private string _display;
-        public string Display
-        {
-            get { return _display; }
-            set { 
-                _display = value;
-                OnPropertyChange("Display");
-            }
-        }
-
-        private string[] imagePaths;
-        public string[] ImagePaths
-        {
-            get { return imagePaths; }
-            set
-            {
-                imagePaths = value;
-                OnPropertyChange("ImagePaths");
-            }
-        }
-
-        private ObservableCollection<string> _folderImages;
-        public ObservableCollection<string> FolderImages
-        {
-            get { return _folderImages; }
-            set
-            {
-                _folderImages = value;
-                OnPropertyChange("FolderImages");
-            }
-        }
-        private void Select_Click(object sender, RoutedEventArgs e)
-        {
-            WinForms.FolderBrowserDialog dialog = new WinForms.FolderBrowserDialog();
-            WinForms.DialogResult result = dialog.ShowDialog();
-
-            if (result == WinForms.DialogResult.OK)
-            {
-                string folder = dialog.SelectedPath;
-
-                string[] allFiles = Directory.GetFiles(folder);
-
-
-                string[] imageExtensions = { ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff" };
-
-                ImagePaths = allFiles.Where(file => imageExtensions.Contains(Path.GetExtension(file).ToLower())).ToArray();
-
-                FolderImages.Clear();
-                foreach(string image in ImagePaths.Select(file => Path.GetFileName(file)).ToArray())
-                    FolderImages.Add(image);
-
-            }
-            else
-            {
-                //MessageBox.Show("Could not open image.", "error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-
-        }
-
-
-        private void Clear_Click(object sender, RoutedEventArgs e)
-        {
-            Display = emty;
-        }
-
-        private void OnPropertyChange(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
+        /*
 
         private void folderImages_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
@@ -113,6 +34,6 @@ namespace ImageViewer
                 Display = emty;
             }
             
-        }
+        }*/
     }
 }
