bb38d2914347855a93c33e7f3bf7e553571f9c5b
FreeSpirit18 <99819303+FreeSpirit18@users.noreply.github.com>
2023-09-11 15:18:37 +0300

    first

--- ImageViewer/ImageViewer/obj/Debug/net7.0-windows/App.g.i.cs
+++ ImageViewer/ImageViewer/obj/Debug/net7.0-windows/App.g.i.cs
@@ -48,7 +48,7 @@ namespace ImageViewer {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -60,7 +60,7 @@ namespace ImageViewer {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/ImageViewer;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ImageViewer;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -74,7 +74,7 @@ namespace ImageViewer {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public static void Main() {
             ImageViewer.App app = new ImageViewer.App();
             app.InitializeComponent();
